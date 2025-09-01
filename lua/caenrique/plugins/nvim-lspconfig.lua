31d8ad131640ef47505db66b7e37d8bd2378c68f
Cesar Enrique Ramirez <caenrique93@gmail.com>
2025-09-01 20:40:38 +0200

    refactor(nvim-lspconfig): update server configuration

--- lua/caenrique/plugins/nvim-lspconfig.lua
+++ lua/caenrique/plugins/nvim-lspconfig.lua
@@ -12,45 +12,44 @@ return {
     { 'j-hui/fidget.nvim', opts = {} },
   },
   config = function()
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    -- local capabilities = vim.lsp.protocol.make_client_capabilities()
 
-    local servers = {
-      lua_ls = {
-        settings = {
-          Lua = {
-            completion = {
-              callSnippet = 'Replace',
-            },
-            diagnostics = { disable = { 'missing-fields', 'duplicate-set-field' } },
-          },
-        },
+    -- local servers = {
+    --   lua_ls = {
+    --     settings = {
+    --       Lua = {
+    --         completion = {
+    --           callSnippet = 'Replace',
+    --         },
+    --         diagnostics = { disable = { 'missing-fields', 'duplicate-set-field' } },
+    --       },
+    --     },
+    --   },
+    -- }
+
+    require('mason-tool-installer').setup({
+      ensure_installed = {
+        'lua_ls',
+        'stylua', -- Used to format Lua code
+        'yaml-language-server',
+        'ts_ls',
+        'jsonls',
+        'pkl-lsp',
+        'gopls',
       },
-    }
-
-    local ensure_installed = vim.tbl_keys(servers or {})
-    vim.list_extend(ensure_installed, {
-      'stylua', -- Used to format Lua code
-      'yaml-language-server',
-      'ts_ls',
-      'jsonls',
-      -- 'marksman',
-      'pkl-lsp',
-      'gopls',
     })
-    require('mason-tool-installer').setup({ ensure_installed = ensure_installed })
 
     require('mason-lspconfig').setup({
       ensure_installed = {}, -- explicitly set to an empty table (installs populated via mason-tool-installer)
-      automatic_installation = false,
       automatic_enable = true,
-      handlers = {
-        function(server_name)
-          local server = servers[server_name] or {}
-          server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-          server.capabilities = require('blink.cmp').get_lsp_capabilities(server.capabilities)
-          require('lspconfig')[server_name].setup(server)
-        end,
-      },
+      -- handlers = {
+      --   function(server_name)
+      --     local server = servers[server_name] or {}
+      --     server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+      --     server.capabilities = require('blink.cmp').get_lsp_capabilities(server.capabilities)
+      --     require('lspconfig')[server_name].setup(server)
+      --   end,
+      -- },
     })
 
     -- pkl-lsp config
--- plugin/keymaps.lua
+++ plugin/keymaps.lua
@@ -1,6 +1,6 @@
 -- Debug keymaps to evaluate lua code of the whole file or visual selection
 vim.keymap.set('n', 'g==', '<cmd>source %<CR>')
-vim.keymap.set({ 'n', 'x' }, 'g==', ":'<,'>source<CR>")
+vim.keymap.set('x', 'g==', ":'<,'>source<CR>")
 
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
