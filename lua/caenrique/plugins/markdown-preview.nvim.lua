890ecb2aeded6b723e3a565b622e3cc8f465b28f
Cesar Enrique Ramirez <caenrique93@gmail.com>
2025-09-17 14:30:18 +0200

    replace markdown-preview with peek.nvim

--- lua/caenrique/plugins/markdown-preview.nvim.lua
+++ lua/caenrique/plugins/markdown-preview.nvim.lua
@@ -1,7 +1,25 @@
 return {
-  'iamcco/markdown-preview.nvim',
-  cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
-  ft = { 'markdown' },
-  build = ':call mkdp#util#install()',
-  init = function() vim.g.mkdp_browser = 'firefox' end,
+  {
+    'iamcco/markdown-preview.nvim',
+    enabled = false,
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    ft = { 'markdown' },
+    build = ':call mkdp#util#install()',
+    init = function() vim.g.mkdp_browser = 'firefox' end,
+  },
+  {
+    'toppair/peek.nvim',
+    event = { 'VeryLazy' },
+    build = 'deno task --quiet build:fast',
+    opts = {
+      theme = 'light',
+      app = 'browser',
+    },
+    config = function(_, opts)
+      require('peek').setup(opts)
+      vim.api.nvim_create_user_command('PeekOpen', require('peek').open, {})
+      vim.api.nvim_create_user_command('PeekClose', require('peek').close, {})
+    end,
+  },
+  {},
 }
