697e3a7f3d501df99c80910ecad813e01d1de070
Cesar Enrique Ramirez <caenrique93@gmail.com>
2025-09-18 12:08:20 +0200

    QoL improvements

--- plugin/autocommands.lua
+++ plugin/autocommands.lua
@@ -1,25 +1,50 @@
+local group = vim.api.nvim_create_augroup('caenrique-events-group', { clear = true })
+
 -- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
---  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  group = group,
   callback = function() vim.highlight.on_yank() end,
 })
 
 -- Show which line your cursor is on only on the current window
-local cursorline_group = vim.api.nvim_create_augroup('cursor-line-auto-command', {})
 vim.api.nvim_create_autocmd({ 'WinEnter', 'InsertLeave' }, {
-  group = cursorline_group,
+  group = group,
   command = 'set cursorline',
 })
 vim.api.nvim_create_autocmd({ 'WinLeave', 'InsertEnter' }, {
-  group = cursorline_group,
+  group = group,
   command = 'set nocursorline',
 })
 
 -- keymap for quickfix list to close it qith 'q'
 vim.api.nvim_create_autocmd('FileType', {
   pattern = 'qf',
+  group = group,
   callback = function() vim.keymap.set('n', 'q', '<CMD>q<CR>', { buffer = true }) end,
+})
+
+-- Return to last edit position when opening files
+vim.api.nvim_create_autocmd('BufReadPost', {
+  group = group,
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then pcall(vim.api.nvim_win_set_cursor, 0, mark) end
+  end,
+})
+
+-- Auto-resize splits when terminal window is resized
+vim.api.nvim_create_autocmd('VimResized', {
+  group = group,
+  callback = function() vim.cmd('tabdo wincmd =') end,
+})
+
+-- Create directories when saving files
+vim.api.nvim_create_autocmd('BufWritePre', {
+  group = group,
+  callback = function()
+    local dir = vim.fn.expand('<afile>:p:h')
+    if vim.fn.isdirectory(dir) == 0 then vim.fn.mkdir(dir, 'p') end
+  end,
 })
