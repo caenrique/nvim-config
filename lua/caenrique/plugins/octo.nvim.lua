8f8f3970f8715a4b60e9b98fb53c8be0c84b2bd4
Cesar Enrique Ramirez <caenrique93@gmail.com>
2025-10-13 13:10:00 +0200

    clean up

--- lua/caenrique/plugins/octo.nvim.lua
+++ lua/caenrique/plugins/octo.nvim.lua
@@ -2,12 +2,9 @@ return {
   'pwntester/octo.nvim',
   dependencies = {
     'nvim-lua/plenary.nvim',
-    -- 'nvim-telescope/telescope.nvim',
-    -- OR 'ibhagwan/fzf-lua',
     'folke/snacks.nvim',
-    -- 'nvim-tree/nvim-web-devicons',
   },
   opts = {
-    picker = 'snacks', -- or "fzf-lua" or "snacks"
+    picker = 'snacks',
   },
 }
